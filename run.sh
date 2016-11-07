92dc148cef15a26435d2d4cbe9e43a97a2ff627b
Darya Filippova <dasha.filippova@gmail.com>
2016-11-07 01:56:56 +0000

    enabled debug output

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 # compile
 rm -f bin/mapper
 make nb-mapper
-alignments=data/chr20/results/nimbliner/sampled_100_10000_m=3.0pct_d=0.1pct.align
-time bin/mapper 20 data/chr20/sampled/sampled_100_10000_m=3.0pct_d=0.1pct.fa data/chr20/chr20.flat.index data/chr20/anchors.sorted.txt > $alignments
-python py-src/stats.py $alignments
+name=data/chr20/results/nimbliner/sampled_100_10000_m=1.0pct_d=0.1pct
+alignments=$name.align
+summary=$name.summary
+debug=$name.debug
+time bin/mapper -k 20 -i data/chr20/sampled/sampled_100_10000_m=1.0pct_d=0.1pct.fa -x data/chr20/chr20.flat.index -a data/chr20/anchors.sorted.txt > $alignments 2> $debug
+python py-src/stats.py $alignments $summary
