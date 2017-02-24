412788fea426a89d101ca3c9a8816dc8295c1002
Darya Filippova <dasha.filippova@gmail.com>
2017-02-24 11:13:09 -0800

    updated tests for the new test genome sequence; baked in the genome seq (9kb)

--- tests/anchor_index_test.cpp
+++ tests/anchor_index_test.cpp
@@ -19,31 +19,34 @@ TEST_CASE("AnchorIndex: read anchors", "[readers]")
         anchor_idx.readAnchors("data/test/hxb2", 20);
         SECTION("test the number of anchors read")
         {
-            REQUIRE(anchor_idx.size() == 194);
+            REQUIRE(anchor_idx.size() == 184);
         }
 
         SECTION("test presence/abscence of certain anchors")
         {
-            REQUIRE(anchor_idx.is_anchor(152413962224) );
+            REQUIRE(anchor_idx.is_anchor(154902124067) );
             REQUIRE(!anchor_idx.is_anchor(0) );
         }
 
         SECTION("test anchor locations")
         {
-            vector<seed_position_t> v1 = anchor_idx.get_anchor_locations(152413962224);
+            vector<seed_position_t> v1 = anchor_idx.get_anchor_locations(154902124067);
             REQUIRE(v1.size() == 1);
-
-            v1 = anchor_idx.get_anchor_locations(912552618219);
-            REQUIRE(v1.size() == 2);
-            // assert sorted order
             seed_position_t seed = v1[0];
             REQUIRE(seed.first == 0);
-            REQUIRE(seed.second == 115);
-            seed = v1[1];
-            REQUIRE(seed.first == 0);
-            REQUIRE(seed.second == 9200);
-            v1 = anchor_idx.get_anchor_locations(0);
-            REQUIRE(v1.size() == 0);
+            REQUIRE(seed.second == 7150);
+
+            // v1 = anchor_idx.get_anchor_locations(912552618219);
+            // REQUIRE(v1.size() == 2);
+            // assert sorted order
+            // seed_position_t seed = v1[0];
+            // REQUIRE(seed.first == 0);
+            // REQUIRE(seed.second == 115);
+            // seed = v1[1];
+            // REQUIRE(seed.first == 0);
+            // REQUIRE(seed.second == 9200);
+            // v1 = anchor_idx.get_anchor_locations(0);
+            // REQUIRE(v1.size() == 0);
         }
     }
 }
